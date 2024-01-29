df2eeaf1e3e827cadefcc9a87f90cfad458cf41a
Njabulo <njabulonkosi59.com>
2024-01-29 20:37:39 +0200

    home page scramle animation added in js

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");   
-}
+function scrambleText(text) {
+    let scrambledText = '';
+    const chars = text.split('');
 
+    while (chars.length) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      scrambledText += chars.splice(randomIndex, 1)[0];
+    }
+
+    return scrambledText;
+  }
+
+  // Function to animate the text scramble
+  function animateTextScramble(element, originalText) {
+    const interval = 150;
+    let currentText = element.innerHTML;
+
+    function updateText() {
+      element.innerHTML = scrambleText(currentText);
+    }
+
+    function completeAnimation() {
+      element.innerHTML = originalText;
+      clearInterval(scrambleInterval);
+    }
+
+    const scrambleInterval = setInterval(updateText, interval);
+
+    
+    setTimeout(completeAnimation, 2000);
+  }
+
+
+  const titleElement = document.querySelector('.title-h1');
+
+ 
+  animateTextScramble(titleElement, titleElement.innerHTML);
